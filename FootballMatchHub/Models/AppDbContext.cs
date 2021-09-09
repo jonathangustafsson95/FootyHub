89b85d1f825f87952b490f661e7e58a2ac2272f8
Jonathan Gustafsson <Jonathan.Gustafsson@sigma.se>
2021-09-09 14:41:50 +0200

    Update of dependencies and enable migrations

---
+++ FootballMatchHub/Models/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace FootballMatchHub.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        //public DbSet<>  { get; set; }
+        //public DbSet<>  { get; set; }
+
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
