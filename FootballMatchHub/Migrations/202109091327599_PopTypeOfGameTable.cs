e8272a9e20306993d27f8432b0fb2a136fa6dd37
Jonathan Gustafsson <Jonathan.Gustafsson@sigma.se>
2021-09-09 15:33:06 +0200

    Populated TypeOfGames db table

---
+++ FootballMatchHub/Migrations/202109091327599_PopTypeOfGameTable.cs
@@ -0,0 +1,20 @@
+﻿namespace FootballMatchHub.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopTypeOfGameTable : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO TypeOfGames (Id, Name) VALUES (1, 'A-Team Game')");
+            Sql("INSERT INTO TypeOfGames (Id, Name) VALUES (2, 'U-Team Game')");
+            Sql("INSERT INTO TypeOfGames (Id, Name) VALUES (3, 'Cup Game')");
+        }
+
+        public override void Down()
+        {
+            Sql("DELETE FROM TypeOfGames WHERE Id IN (1,2,3)");
+        }
+    }
+}
